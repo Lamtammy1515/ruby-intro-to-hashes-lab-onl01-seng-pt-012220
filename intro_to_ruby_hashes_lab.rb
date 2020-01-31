b18d00a3d8be9bde19dfc37f411136b4ec40c298
Tammy Lam <lamtammy1515@gmail.com>
2020-01-31 03:32:21 +0000

    Automatically backed up by Learn

--- intro_to_ruby_hashes_lab.rb
+++ intro_to_ruby_hashes_lab.rb
@@ -34,7 +34,8 @@ monopoly = {
   :pieces => 4,
   :names => {
     :reading_railroad =>{},
-:pennsylvania_railroad =>{}
+:pennsylvania_railroad =>{},
+:b_and_o_railroad =>{}
   },
   :rent_in_dollars => {
     :one_piece_owned =>25,
